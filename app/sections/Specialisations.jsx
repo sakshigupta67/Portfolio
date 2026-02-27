456dc004fd3ff346e977e6729ca775f16e77ef3f
Sakshi Gupta <sakshimailara@gmail.com>
2026-02-27 22:03:44 +0530

    Add "Computer Network" to Specialisations list

--- app/sections/Specialisations.jsx
+++ app/sections/Specialisations.jsx
@@ -18,6 +18,7 @@ export default function Specialisations() {
     { title: "Operating Systems" },
     { title: "Software Engineering" },
     { title: "Database Management" },
+    { title: "Computer Network" },
   ];
 
   return (
